e6351a4eb96808c4d667fa0c6b066fb04ca98054
Jurrian Lammerts <0914922@hr.nl>
2017-11-22 23:01:11 +0100

    Added first controller

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('about', 'PagesController@getContact');
+
+Route::get('contact', 'PagesController@getContact');
+
+Route::get('/', 'PagesController@getIndex');
