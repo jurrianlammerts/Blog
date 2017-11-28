f5b4da9755c53cb288aeebd596a166c2087994d4
Jurrian Lammerts <0914922@hr.nl>
2017-11-29 00:09:26 +0100

    Basic CRUD

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('about', 'PagesController@getAbout');
 Route::get('contact', 'PagesController@getContact');
 
 Route::get('/', 'PagesController@getIndex');
+
+Route::resource('posts', 'PostController');
